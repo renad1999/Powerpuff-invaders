1c0f1e09bb5e9ac4c2ab87e85ee660b58ecf8e9b
renad1999 <re0016@outlook.com>
2023-05-11 11:39:19 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,42 @@
+// Constants
+// blossom shooter
+// mojo jojo
+// number of rows and colu
+// Number of rows and columns in the page
+// the screen height and width in pixels 
+// player speed
+// enemy speed
+
+// State variables
+// Keep track of how many bad guys are killed
+// keep track of shooter player and if they are killed, decrease lives
+// keep track of score
+// make sure shooter doesnt touch enemies if so GAME OVER
+// keep track of enemy if he reaches ground its game OVER
+// GAME 
+
+
+// Cached elements
+
+
+
+// Event listeners
+
+
+
+// Functions
+
+
+
+
+// Make sure code is checking users moves
+// the enemy is moving left to right and moving downwards
+// keep track of shooter shooting fire at enemey
+// keep track of how many shots went on enemy
+// keep track if enemy gets to bottom of page, end game
+// keep track if enemy hits the shooter, end game 
+// if game is won = GO POWERPUFF
+// if game is lost = MOJOJOJO WON!
+// Render values to work 
+// Wait for player to start moving player
+// Replay button 
